008b20db194963c7dea00d54cb57291cb6fce54c
isabellyluz <isabellyluz19@gmail.com>
2025-10-14 20:30:17 -0300

    uso de if e else

--- aula_14_10/main.dart
+++ aula_14_10/main.dart
@@ -1,10 +1,12 @@
 void main() {
 String Nome = 'Isabelly';
-int Idade = 14;
+int Idade = 18;
 double Altura = 1.65;
 bool Resultado = 10>5;
-print(Nome);
-print(Idade);
-print(Altura);
-print(Resultado);
+
+  if (Idade >= 18) {
+    print('Você é maior de idade');
+  } else { 
+    print('Você é menor de idade');
+  }
 }
